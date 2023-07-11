f8af093038cc7dca84c0038dccbda5498477f2ad
290601 <13521028@std.stei.itb.ac.id>
2023-07-11 20:39:23 +0700

    add day_8

--- day_8/js/testimonials.js
+++ day_8/js/testimonials.js
@@ -101,6 +101,10 @@ const testimonial2 = new UserTestimonial("Ruby Hoshino", "Mantab banget oni-chan
 
 const testimonial3 = new CompanyTestimonial("Umbrella", "Boleh juga, bagi dulu virus Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci deleniti incidunt magni assumenda, quos voluptas enim saepe dolorum nulla amet, sed dolorem odit, doloribus itaque molestiae molestias exercitationem. Vero, deserunt.", "https://e7.pngegg.com/pngimages/484/820/png-clipart-umbrella-corps-umbrella-corporation-logo-corporation-s-umbrella-triangle.png")
 
+// const testimonial4 = new Testimonial("Apasih ga jelas", "https://images.unsplash.com/photo-1578632767115-351597cf2477?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80")
+
+// console.log(testimonial1, testimonial2, testimonial3)
+
 let testimonialData = [testimonial1, testimonial2, testimonial3]
 
 let testimonialHTML = ""
